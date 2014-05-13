bdcc453b6efd5143518a8475ad4ae7f45ebfb8aa
Troy Whorten <troy.whorten@wyo.gov>
2014-05-13 16:09:12 -0600

    another change

--- OdeToFoodGit/Scripts/otf.js
+++ OdeToFoodGit/Scripts/otf.js
@@ -17,11 +17,21 @@
         return false;
     };
 
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
+    };
+
+
     var createAutocomplete = function () {
         var $input = $(this);
 
         var options = {
-            source: $input.attr("data-otf-autocomplete")
+            source: $input.attr("data-otf-autocomplete"),
+            select: submitAutocompleteForm
         };
 
         $input.autocomplete(options);
